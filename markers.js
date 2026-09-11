7dbdfd0fcd16f9fc9666219236189eb350b3f076
RoBorg87 <roccobottani@outlook.com>
2026-09-11 18:45:30 +0200

    Add files via upload

---
+++ markers.js
@@ -0,0 +1,38 @@
+var poi = [
+    {
+        nome: 'Nella Tana del Bisciòn di Brena',
+        x: 600,
+        y: 450,
+        descrizione: 'Un piccolo villaggio e un enorme albero custode, stravolti da un oscura leggenda.'
+    },
+    {
+        nome: 'Nel Sogno della Torre d\'Inverno',
+        x: 750,
+        y: 300,
+        descrizione: 'Una faida vecchia di un secolo sopita, ma non spenta, sotto un manto di rancore e magia.'
+    },
+    {
+        nome: 'Misteriose Scomparse al Villaggio di Rima',
+        x: 750,
+        y: 300,
+        descrizione: 'Un villaggio il cui destino è ormai segnato... ma forse qualcuno può ancora ritrovare la speranza.'
+    },
+    {
+        nome: 'Il Tesoro oltre il Ponte del Diavolo',
+        x: 750,
+        y: 300,
+        descrizione: 'La brama e la scaltrezza dei nani fu la loro rovina. Ma ora nuove forze giocano con la loro eredità.'
+    },
+    {
+        nome: 'Fuga dalla Biblioteca dei Mondi',
+        x: 750,
+        y: 300,
+        descrizione: 'Un luogo mutevole e impossibile, racchiuso tra le pagine di un libro creato per non essere mai terminato.'
+    },
+    {
+        nome: 'Occhi Rossi nelle Acque Nere',
+        x: 528,
+        y: 438,
+        descrizione: 'Un furto e un omicidio in un luogo in cui fidarsi di chiunque sembra impossibile.'
+    }
+];
